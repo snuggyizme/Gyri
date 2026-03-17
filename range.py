eb373bc1ee8ff3fd864d1253e459886297d5b828
snuggyizme <snuggyizme@gmail.com>
2026-03-17 20:29:59 +1100

    More range work and removal of 2D ranges

--- range.py
+++ range.py
@@ -4,61 +4,44 @@ from interpreter import _throw
 class Range:
     def __init__(self, head: tuple, tail: tuple, tape: Tape, interpreterIndex: int):
         """
-        Creates a custom type called the 'Range'. A range is a 1D or 2D ordered group of cell values. It is defined by the <head> and <tail> coordinates.
-        Internally, a range is a list of the values of the cells inside the head-tail pair. It is ordered X first, and 2D ranges either go up (Y+) or down (Y-)
-        based off if the head is lower or higher than the tail respectively. Ranges cannot be easily stored in a integer/cellvalue and are temporary.
+        Creates a custom type called the 'Range'. A range is an ordered group of cell values. It is defined by the <head> and <tail> coordinates.
+        Internally, a range is a list of the values of the cells inside the head-tail pair. Ranges can be made in any direction (e.g. @0,0 to @0,5 or @5,0 to @-5,0)
+        Ranges cannot be easily stored in a integer/cellvalue and are temporary.
         """
         self.head = head
         self.tail = tail
 
-        self.direction = ""
-        self.twoDimensional: bool
-        self.rows = 0
-        self.columns = 0
+        self.length = 0
 
         if self.head == self.tail:
             _throw("Error creating range: head coordinate cannot be the same as tail", interpreterIndex) 
         
         if head[0] == tail[0] and head[1] != tail[1]:
-            self.twoDimensional = False
-
             if head[1] > tail[1]:
-                self.direction = "down"
-                self.rows = head[1] - tail[1]
-                self.columns = 1
+                self.indexRanges = (head[0], range(head[1], tail[1] + 1))
             else:
-                self.direction = "up"
-                self.rows = tail[1] - head[1]
-                self.columns = 1
-        
+                self.indexRange = (head[0], range(tail[1], head[1] + 1))
         elif head[0] != tail[0] and head[1] == tail[1]:
-            self.twoDimensional = False
-
             if head[0] > tail[0]:
-                self.direction = "left"
-                self.rows = head[0] - tail[0]
-                self.columns = 1
+                self.indexRange = (range(head[0], tail[0] + 1), head[1])
             else:
-                self.direction = "right"
-                self.rows = tail[0] - head[0]
-                self.columns = 1
-        
+                self.indexRange = (range(tail[0], head[0] + 1), head[1])
         else:
-            self.twoDimensional = True
-
-            if head[1] > tail[1]:
-                self.direction = "down"
-                self.rows = tail[1] - head[1]
-                self.columns = tail[0] - head[0]
-            else:
-                self.direction = "up"
-                self.rows = tail[1] - head[1]
-                self.columns = head[0] - tail[0]
+            _throw("Error creating range: range coordinates are not aligned along a dimension", interpreterIndex)
         
         self.values = []
 
-        # I will do 2D later
-        for row in self.rows:
-            self.values.append(tape.get(row))
-            
-        
+        for x, y in self.indexRange:
+            self.values.append(tape.get((x, y)))
+
+    def getAll(self):
+        """
+        Simply returns the range as a list.
+        """
+        return self.values
+    
+    def get(self, index):
+        """
+        Get the value at <index>
+        """
+        return self.values[index]
